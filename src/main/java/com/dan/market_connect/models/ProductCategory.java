e54a71d8626cf5bee47e7419ece880ead1f98f7f
Dan <inb3zna@gmail.com>
2024-01-09 21:06:23 +0200

    added entities and ProductCategory enum

---
+++ src/main/java/com/dan/market_connect/models/ProductCategory.java
@@ -0,0 +1,9 @@
+package com.dan.market_connect.models;
+
+public enum ProductCategory {
+
+    TECH,
+    FITNESS,
+    BOOKS,
+    GARDENING
+}
---
+++ src/main/java/com/dan/market_connect/models/entities/Product.java
@@ -0,0 +1,43 @@
+package com.dan.market_connect.models.entities;
+
+import com.dan.market_connect.models.ProductCategory;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @ElementCollection
+    @CollectionTable(name = "customer_ratings",
+            joinColumns = @JoinColumn(name = "customer_id"))
+    @MapKeyColumn(name = "email")
+    @Column(name = "ratings")
+    private Map<Customer, Double> customerRatings;
+
+    @ElementCollection
+    @CollectionTable(name = "customer_reviews",
+            joinColumns = @JoinColumn(name = "customer_id"))
+    @MapKeyColumn(name = "email")
+    @Column(name = "reviews")
+    private Map<Customer, Double> customerReviews;
+
+    @Column(name = "product_price")
+    private Double productPrice;
+
+    @Column(name = "product_category")
+    private ProductCategory productCategory;
+
+    @Column(name = "product_description")
+    private String productDescription;
+}
